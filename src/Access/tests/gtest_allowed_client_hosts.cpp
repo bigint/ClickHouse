d3766cd3518f5715dfbc318ef175439e5a66b732
Bigint <69431456+bigint@users.noreply.github.com>
2026-08-22 22:02:52 +0530

    fix(access): recognize short IPv6 host patterns

---
+++ src/Access/tests/gtest_allowed_client_hosts.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include <Access/Common/AllowedClientHosts.h>
+
+
+using namespace DB;
+
+TEST(AllowedClientHosts, IPv6LikePatternAllowsShortFirstGroup)
+{
+    AllowedClientHosts hosts;
+    hosts.addLikePattern("65:ff0c::/96");
+
+    EXPECT_TRUE(hosts.contains(AllowedClientHosts::IPAddress{"65:ff0c::1"}));
+    EXPECT_FALSE(hosts.contains(AllowedClientHosts::IPAddress{"65:ff0d::1"}));
+}
+
+TEST(AllowedClientHosts, IPv6WildcardAllowsShortFirstGroup)
+{
+    AllowedClientHosts hosts;
+    hosts.addLikePattern("a::%");
+
+    EXPECT_TRUE(hosts.contains(AllowedClientHosts::IPAddress{"a::1234"}));
+    EXPECT_FALSE(hosts.contains(AllowedClientHosts::IPAddress{"b::1234"}));
+}
